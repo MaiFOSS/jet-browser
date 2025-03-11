72d4450e8f49da0661933c3403879bf4d6905758
amirali-dashti <damirali298@gmail.com>
2025-03-11 23:06:44 +0330

    deleted:    .idea/jet-browser.iml 	deleted:    src/JetBrowser/.idea/.gitignore 	deleted:    src/JetBrowser/.idea/misc.xml 	deleted:    src/JetBrowser/.idea/modules.xml 	deleted:    src/JetBrowser/.idea/vcs.xml 	deleted:    src/JetBrowser/JetBrowser.iml 	new file:   src/jet-engine/Cargo.toml 	new file:   src/jet-engine/output/google.html 	new file:   src/jet-engine/src/main.rs

---
+++ src/jet-engine/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use std::path::Path;
+use web_archive::blocking;
+
+fn main() -> std::io::Result<()> {
+    let archive = blocking::archive("https://www.google.com", Default::default())
+        .expect("Failed to archive the page");
+    let page = archive.embed_resources();
+    
+    let output_dir = Path::new("output");
+    fs::create_dir_all(output_dir)?;
+
+    let file_path = output_dir.join("google.html");
+
+    fs::write(&file_path, page)
+        .expect("Failed to write the output to file");
+
+    println!("Output saved to {:?}", file_path);
+    Ok(())
+}
